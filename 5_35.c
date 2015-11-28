61548a419199bd6286cbad934e0b174870734d86
scensorECHO <xxartofwarxx@gmail.com>
2015-11-28 18:20:40 -0500

    Fibonacci maximum possible for unsigned long long int data types output complete

--- 5_35.c
+++ 5_35.c
@@ -1,10 +1,37 @@
 #include <stdio.h>
 
-
+unsigned long long int fibonacci(unsigned int t);
+unsigned long long int maxim_fib();
 int main(){
-	// Testing maximum value [18446744073709551615]
-	unsigned long long int i = (unsigned long long int) -1;
-	printf("%llu\n", i);
+	unsigned int term;
+	printf("What term of the Fibonacci sequence would you like to retrieve? ");
+	scanf("%u",&term);
+	printf("\nTerm %u of the Fibonacci sequence is %llu",term, fibonacci(term));
+	maxim_fib();
+}
 
+unsigned long long int fibonacci(unsigned int t){
+	unsigned long long int _1 = 0;
+	unsigned long long int _2 = 1;
+	unsigned long long int tmp;
+	for(unsigned int i=0;i<t;i++){
+		tmp=_2;
+		_2+=_1;
+		_1=tmp;
+	}
+	return _2;
+}
 
+unsigned long long int maxim_fib(){
+	unsigned long long int _1 = 1;
+	unsigned long long int _2 = 1;
+	unsigned long long int tmp;
+	unsigned int i = 0;
+	while(_2>=_1){
+		tmp=_2;
+		_2+=_1;
+		_1=tmp;
+		i++;
+	}
+	printf("\n\nThe largest Fibonacci value that can be retrieved with this program is %llu, which is term %u\n",_1,i);
 }
