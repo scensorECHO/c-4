4b89ff95652e58fd6a54e7ee2ac9df78b5712a1a
scensorECHO <xxartofwarxx@gmail.com>
2015-11-28 17:10:30 -0500

    Updated to use sprintf to parse integer as an char array

--- 5_22.c
+++ 5_22.c
@@ -6,47 +6,24 @@ int user_b;
 
 int divide(int _a, int _b);
 int remain(int _a, int _b);
-int get_length(int _a);
 int main(){
-	
 	printf("Enter an integer\n");
 	scanf("%d",&user_a);
 	printf("Enter another integer\n");
 	scanf("%d",&user_b);
-
+	char* user_a_str;
+	sprintf(user_a_str,"%d",user_a);
 	printf("The integer quotient of your integers is %d\n",divide(user_a,user_b));
 	printf("The integer remainder of your integer is %d\n",remain(user_a,user_b));
 	printf("Separating each number in the first entry:");
-	int len = get_length(user_a);
-	printf("len is %d",len);
-	int separated[len];
-	int d=user_a;
-	for(int x=len-1; x>=0; x++){
-		separated[x]=d%10;
-		d/=10;
+	for(int x=0; x<sizeof(user_a_str); x++){
+		printf("  %c",user_a_str[x]);
 	}
 
-	for(int y=0; y<len; y++){
-		printf("  %1d",separated[y]);
-	}
 }
-
 int divide(int _a, int _b){
 	return _a/_b;
 }
-
 int remain(int _a, int _b){
 	return _a%_b;
-}
-
-int get_length(int _a){
-	if(_a==0){
-		return 1;
-	} else {
-		int c = 0;
-		while(_a!=0){
-			_a/=10;
-			c++;
-		}
-	}
 }
